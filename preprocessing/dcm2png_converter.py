b4edb3ae2130d286ae3d6ed73f45bc1e72ad59e1
yujinkim <yujin7621@me.com>
2022-11-22 13:21:03 +0900

    refactor: Rename dcm2png_converter

--- preprocessing/dcm2png_converter.py
+++ preprocessing/dcm2png_converter.py
@@ -6,7 +6,7 @@ def main():
     parser.add_argument('--original_path', type=str, required=True,
                         help='directory of original folder to convert dcm format to png format')
     parser.add_argument('--convert_path', type=str, required=True,
-                        help='directory of converting folder to convert dcm format to png format'')
+                        help='directory of converting folder to convert dcm format to png format')
     args = parser.parse_args()
 
     mritopng.convert_folder(args.original_path, args.convert_path)
